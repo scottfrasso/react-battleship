cda83e53c484b0823cd5a18d693261db331f93d9
Scott Frasso <scottfrasso@gmail.com>
2022-08-16 18:52:29 +0200

    Added a modal for when the game is over.

---
+++ src/components/Modal.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import classes from './Modal.module.css'
+
+type BackdropProps = {
+  onClose: (e: React.MouseEvent) => void
+}
+
+const Backdrop = (props: BackdropProps) => {
+  return <div className={classes.backdrop} />
+}
+
+type ModalOveralyProps = {
+  children?: React.ReactNode
+}
+
+const ModalOverlay = (props: ModalOveralyProps) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  )
+}
+
+const portalElement = document.getElementById('overlays') as HTMLElement
+
+type Props = {
+  onClose: (e: React.MouseEvent) => void
+  children?: React.ReactNode
+}
+
+const Modal = (props: Props) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </React.Fragment>
+  )
+}
+
+export default Modal
--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
+import Modal from './Modal'
+
 type PropsType = {
   gameOver: boolean
   onResetClick: (e: React.MouseEvent) => void
 }
 const Status = ({ gameOver, onResetClick }: PropsType) => {
+  const modalContent = (
+    <>
+      <h1>You won!</h1>
+      <button onClick={onResetClick}>Reset</button>
+    </>
+  )
+
   return (
     <>
-      {!gameOver && <p>Keep Playing!</p>}
-      {gameOver && <p>Game Over! You won!</p>}
+      {gameOver && <Modal onClose={onResetClick}>{modalContent}</Modal>}
       <button onClick={onResetClick}>Reset</button>
     </>
   )
